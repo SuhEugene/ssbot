85950ffccf15e804d740a6dbb72a6cf1eabc26c0
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2024-09-01 21:48:58 +0300

    Set view to details

--- servePlugs.js
+++ servePlugs.js
@@ -3,7 +3,7 @@ const serveIndex = require('serve-index');
 
 const app = express();
 
-app.use('/plugs', express.static('plugs'), serveIndex('plugs', { icons: true }));
+app.use('/plugs', express.static('plugs'), serveIndex('plugs', { icons: true, view: 'details' }));
 app.listen(3000, () => {
   console.log('Serving plugs on 3000!');
 });
